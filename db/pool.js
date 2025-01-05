1995d81321c7fe77576332146e378f43a09e2757
ggdevlaptop <gonzalo.a.gomez22@gmail.com>
2025-01-05 03:06:42 -0300

    Change env variable name

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,18 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  user: process.env.USERNAME,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: 5432,
+const pool = new Pool({
+  connectionString: process.env.PRODUCTION_URL,
 });
+
+module.exports = pool;
+
+// const { Pool } = require("pg");
+
+// module.exports = new Pool({
+//   user: process.env.USERNAME,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: 5432,
+// });
